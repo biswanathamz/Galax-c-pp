35ac9cee7b55a434c49313cced4d51c20f6be3ef
biswanathamz <biswanathamz@gmail.com>
2020-07-21 09:01:21 +0530

    Linked list added

---
+++ DS/LinkedList/LinkedList using struct/singleln.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+    int data;
+    struct node *next;
+};
+void insert_node_at_end(struct node **head,int data){
+    struct node *temp;
+    struct node *newNode;
+    newNode = (struct node*) malloc(sizeof(struct node));
+    newNode->next = NULL;
+    newNode->data = data;
+    if(*head==NULL){
+        *head = newNode;
+    }
+    else{
+        temp = *head;
+        while(temp->next!=NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+    cout<<data<<" is added into list, Position : at END"<<endl;
+}
+void displayLL(struct node *head){
+    struct node *temp;
+    if(head == NULL){
+        cout<<"Linked List is empty"<<endl;
+    } 
+    else{
+        cout<< "Printing elements of linked list : ";
+        temp = head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+    cout<<endl;
+}
+int main(){
+    int input,valinput,loopBreak=0;
+    struct node *head;
+    while(1){
+        cout<<"Menu : (Enter accordingly)"<<endl;
+        cout<<"Enter 1 to a node at the end of a linked list"<<endl;
+        cout<<"Enter 5 to display linked list"<<endl;
+        cin>>input;
+        switch(input){
+            case 1:
+                cout<<"Enter the integer value to add in the list : ";
+                cin>>valinput;
+                insert_node_at_end(&head,valinput);
+                break;
+            case 5:
+                displayLL(head);
+                break;
+            default:
+                loopBreak = 1;
+        }
+        if(loopBreak){
+            break;
+        }
+    }
+    return 1;
+}
